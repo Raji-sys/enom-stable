3b878fcfadb61a9b2d0343f4e8ec803646acce5e
Muhammad Raji <rajisunusi@gmail.com>
2023-12-03 22:32:30 +0100

    ready for test

--- staff/models.py
+++ staff/models.py
@@ -7,19 +7,12 @@ from django.db import models
 from django.urls import reverse
 from django.contrib import messages
 
-
-
-def valMax(v):
-    mdigits=4
-    vstr=str(v)
-    if len(vstr)>mdigits:
-        raise ValidationError(f'Maximum {mdigits} digits allowed')
     
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     email=models.EmailField(blank=True, null=True, max_length=100, unique=True)
     photo = models.ImageField(null=True)
-    file_no = models.PositiveIntegerField('file number', null=True, unique=True, blank=False, validators=[valMax, MaxValueValidator(999999)])
+    file_no = models.DecimalField('file number', max_digits=6, decimal_places=0, null=True, unique=True, blank=False)
     title = models.CharField(max_length=300, null=True, blank=True)
     sex=(('MALE','MALE'),('FEMALE','FEMALE'))
     gender = models.CharField(choices=sex, max_length=10, null=True, blank=True)
@@ -59,6 +52,7 @@ class Profile(models.Model):
     snok_rel = models.CharField('second next of kin', max_length=300, null=True, blank=True)
     snok_photo = models.ImageField(null=True)
     timestamp = models.DateTimeField('date added', auto_now_add=True)
+    created = models.DateTimeField('date added', auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def get_absolute_url(self):
@@ -69,30 +63,30 @@ class Profile(models.Model):
             return f"{self.user.first_name} {self.user.last_name}"
 
     def age(self):
-        today=date.today()
+        today = date.today()
         if self.dob:
-            age=today.year-self.dob.year
+            age = today.year - self.dob.year
 
-            if today.month<self.dob.month or (today.month==self.dob.month and today.day < self.dob.day):
-                age-=1        
+            if today.month < self.dob.month or (today.month == self.dob.month and today.day < self.dob.day):
+                age -= 1
             return age
     
-    def dob(self):
-        today=date.today()
-        if self.dob is not None:
-            is_dob=today.month==self.dob.month and today.day ==self.dob.day
-            if is_dob:
-                return is_dob
+    def is_birthday(self):
+        today = date.today()
+        if self.dob:
+            return today.month == self.dob.month and today.day == self.dob.day
+        return False
 
 
 class Qualification(models.Model):
     user = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name='qual')
-    school = models.CharField(max_length=300,null=True, blank=True)
-    types=(('PRIMARY','PRIMARY'),('SECONDARY','SECONDARY'), ('COLLEGE OF EDUCATION','COLLEGE OF EDUCATION'),('POLYTECHNIC','POLYTECHNIC'),('UNIVERSITY','UNIVERSITY'))
-    school_category=models.CharField(choices=types, max_length=300, null=True, blank=True)
-    qual = models.CharField('qualification',max_length=300,null=True,blank=True)    
-    date_obtained = models.DateField(null=True,blank=True)
-    timestamp = models.DateTimeField('date added', auto_now_add=True)
+    school = models.CharField(max_length=300, null=True, blank=True)
+    types = (('PRIMARY', 'PRIMARY'), ('SECONDARY', 'SECONDARY'), ('COLLEGE OF EDUCATION', 'COLLEGE OF EDUCATION'),
+             ('POLYTECHNIC', 'POLYTECHNIC'), ('UNIVERSITY', 'UNIVERSITY'))
+    school_category = models.CharField(choices=types, max_length=300, null=True, blank=True)
+    qual = models.CharField('qualification', max_length=300, null=True, blank=True)
+    date_obtained = models.DateField(null=True, blank=True)
+    created = models.DateTimeField('date added', auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def get_absolute_url(self):
@@ -100,16 +94,15 @@ class Qualification(models.Model):
 
     def __str__(self):
         if self.user:
-            return f"{self.user.first_name} {self.user.last_name}"
-
+            return f"{self.user.first_name} {self.user.last_name} - {self.qual}"
 
 class ProfessionalQualification(models.Model):
     user = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name='pro_qual')
-    institute = models.CharField(max_length=300,null=True,blank=True)
-    inst_address = models.CharField('institute address', null=True,max_length=300,blank=True)
-    qual_obtained = models.CharField('qualification obtained',null=True,max_length=300,blank=True)
-    date_obtained = models.DateField(null=True,blank=True)
-    timestamp = models.DateTimeField('date added',auto_now_add=True)
+    institute = models.CharField(max_length=300, null=True, blank=True)
+    inst_address = models.CharField('institute address', null=True, max_length=300, blank=True)
+    qual_obtained = models.CharField('qualification obtained', null=True, max_length=300, blank=True)
+    date_obtained = models.DateField(null=True, blank=True)
+    created = models.DateTimeField('date added', auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
    
     def get_absolute_url(self):
@@ -117,13 +110,9 @@ class ProfessionalQualification(models.Model):
 
     def __str__(self):
         if self.user:
-            return f"{self.user.first_name} {self.user.last_name}"
+            return f"{self.user.first_name} {self.user.last_name} - {self.qual_obtained}"
 
-def vMax(v):
-    mdigits=6
-    vstr=str(v)
-    if len(vstr)>mdigits:
-        raise ValidationError(f'Maximum {mdigits} digits allowed')
+
 
 class GovernmentAppointment(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
@@ -159,7 +148,7 @@ class GovernmentAppointment(models.Model):
          )
     department=models.CharField(choices=dep, blank=True,max_length=300, null=True)
     cpost=models.CharField('current post',blank=True,max_length=300, null=True)
-    ippis_no = models.PositiveIntegerField('IPPIS number', null=True, unique=True, blank=True,validators=[vMax,MaxValueValidator(999999)])
+    ippis_no = models.DecimalField('IPPIS number', max_digits=6, decimal_places=0, null=True, unique=True, blank=False)
     date_fapt = models.DateField('date of first appointment', null=True,blank=True)
     date_capt = models.DateField('date of current appointment', null=True,blank=True)
     tp=(('CASUAL','CASUAL'),('LOCUM','LOCUM'),('PERMANENT','PERMANENT'),('PROBATION', 'PROBATION'))
@@ -174,7 +163,7 @@ class GovernmentAppointment(models.Model):
     tc=(('JUNIOR','JUNIOR'), ('SENIOR','SENIOR'),('EXECUTIVE','EXECUTIVE'))
     type_of_cadre=models.CharField(choices=tc, null=True, blank=True, max_length=100)
     exams_status = models.CharField(null=True, blank=True, max_length=100)
-    timestamp = models.DateTimeField('date added', auto_now_add=True)
+    created = models.DateTimeField('date added', auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def get_absolute_url(self):
@@ -186,24 +175,12 @@ class GovernmentAppointment(models.Model):
 
     #step calculation
     def step_inc(self):
-        today=date.today()
-        if self.step is not None:
+       today = date.today()
+       if self.step is not None:
             if today.month == 1 and today.day == 1:
-                self.step +=1
+                self.step += 1
+                self.save()
          
-
-class Promotion(models.Model):
-    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name='promotion')
-    cpost = models.CharField('current post',null=True,max_length=300,blank=True)
-    govapp=models.ForeignKey(GovernmentAppointment, on_delete=models.CASCADE, related_name='progovapp')
-    prom_date = models.DateField('promotion date',null=True,blank=True)
-    gl = models.PositiveIntegerField('grade level',null=True,blank=True)
-    step = models.PositiveIntegerField(null=True,blank=True)
-    inc_date = models.DateField('increment date', null=True,blank=True)
-    conf_date = models.DateField('confirmation date', null=True,blank=True)
-    due=models.BooleanField(null=True, blank=True)
-    timestamp = models.DateTimeField('date added', auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
 
     def get_absolute_url(self):
         return reverse('prom_details', args=[self.user])
@@ -213,52 +190,71 @@ class Promotion(models.Model):
             return f"{self.user.last_name} {self.user.first_name}"
 
     #promotion calculation
-    def prom_cal(self,request):
-        today=date.today()
+class Promotion(models.Model):
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name='promotion')
+    cpost = models.CharField('current post', null=True, max_length=300, blank=True)
+    govapp = models.ForeignKey(GovernmentAppointment, on_delete=models.CASCADE, related_name='progovapp')
+    prom_date = models.DateField('promotion date', null=True, blank=True)
+    gl = models.PositiveIntegerField('grade level', null=True, blank=True)
+    step = models.PositiveIntegerField(null=True, blank=True)
+    inc_date = models.DateField('increment date', null=True, blank=True)
+    conf_date = models.DateField('confirmation date', null=True, blank=True)
+    due = models.BooleanField(default=False)
+    created = models.DateTimeField('date added', auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
-        if self.govapp.date_capt is not None:
-            cal=self.govapp.date_capt.year
-            ex=self.govapp.exams_status
-            gl=self.govapp.grade_level
-            tc=self.govapp.type_of_cadre
-            
-            #performing checks
-            if cal is not None:
-                if today.year-cal  == 3 and int(gl) >= 6 and ex == 'pass' and tc == 'SENIOR':
-                    self.due = True  
-                    self.save()
-                    return messages.success(request,'DUE FOR PROMOTION')
-                elif today.year-cal  == 2 and int(gl) <= 5 and ex == 'pass' and tc == 'JUNIOR':
-                    self.due = True    
-                    self.save()
-                    return messages.success(request,'DUE FOR PROMOTION')
-                elif today.year-cal  == 4 and int(gl) >= 13 and ex == 'pass' and tc == 'EXECUTIVE':
-                    self.due = True    
-                    self.save()
-                    return messages.success(request,'DUE FOR PROMOTION')
-                else:
-                    self.due = False
-                    self.save()
+    def get_absolute_url(self):
+        return reverse('prom_details', args=[self.user])
+
+    def __str__(self):
+        if self.user:
+            return f"{self.user.last_name} {self.user.first_name}"
+
+        # Promotion calculation
+    def calculate_promotion(self, request):
+        today = date.today()
+
+        if self.govapp.date_capt:
+            cal = self.govapp.date_capt.year
+            ex = self.govapp.exams_status
+            gl = self.govapp.grade_level
+            tc = self.govapp.type_of_cadre
+
+        # Performing checks
+        if (
+            (today.year - cal == 3 and int(gl) >= 6 and ex == 'pass' and tc == 'SENIOR') or
+            (today.year - cal == 2 and int(gl) <= 5 and ex == 'pass' and tc == 'JUNIOR') or
+            (today.year - cal == 4 and int(gl) >= 13 and ex == 'pass' and tc == 'EXECUTIVE')
+            ):
+            self.due = True
+            self.save()
+            return messages.success(request, 'DUE FOR PROMOTION')
+
+        # If not due, no need to send a message
+        self.due = False
+        self.save()
+        return None
 
 
 class Discipline(models.Model):
     user = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name='discipline')
-    offense = models.TextField(null=True,blank=True)
-    decision = models.TextField(null=True,blank=True)
-    date = models.DateField(null=True,blank=True)
-    comment = models.TextField(null=True,blank=True)
-    timestamp = models.DateTimeField('date added', auto_now_add=True)
+    offense = models.TextField(null=True, blank=True)
+    decision = models.TextField(null=True, blank=True)
+    action_date = models.DateField('date of disciplinary action', null=True, blank=True)
+    comment = models.TextField(null=True, blank=True)
+    created = models.DateTimeField('date added', auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def get_absolute_url(self):
         return reverse('dis_details', args=[self.user])
 
     class Meta:
-        verbose_name_plural='Disciplinaries'
+        verbose_name_plural = 'Disciplinaries'
     
     def __str__(self):
         if self.user:
             return f"{self.user.last_name} {self.user.first_name}"
+
 
 
 class Leave(models.Model):
@@ -270,9 +266,9 @@ class Leave(models.Model):
     balance=models.PositiveIntegerField(null=True,blank=True)
     granted = models.PositiveIntegerField('number of days granted',null=True, blank=False)
     status = models.CharField(null=True,max_length=300,blank=True)
-    lv=models.BooleanField(blank=True, null=True)
+    is_leave_over=models.BooleanField(default=False)
     comments = models.TextField('comments if any', null=True,blank=True)
-    timestamp = models.DateTimeField('date added', auto_now_add=True)
+    created = models.DateTimeField('date added', auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def get_absolute_url(self):
@@ -288,32 +284,30 @@ class Leave(models.Model):
     
     def remain(self):
         if self.total_days is not None and self.granted is not None:
-            return self.total_days - self.granted
+            return max(0, self.total_days - self.granted)
 
     def clean(self):
         super().clean()
 
         r = self.remain()
-        if r <= 0 or self.granted == 0:
-            raise ValidationError('your leave is over.')
-        
-    @property    
+        if r < 0 or self.granted == 0:
+            raise ValidationError('Your leave is over or no days granted.')
+
     def return_on(self):
-        if self.granted is not None:
-            if self.granted > 0:
-                ddays=self.total_days-self.remain()
-                timedelta_o=timedelta(days=ddays)
-                new_date=self.start_date+timedelta_o
-                if new_date is not None:
-                    return new_date
-    
-    def over(self):        
-        today=date.today()
-        if self.remain() is not None and self.remain() <= 0 and today == self.return_on:
-            self.lv = True
+        if self.granted is not None and self.granted > 0:
+            return self.start_date + timedelta(days=self.total_days - self.remain())
+        return None
+
+    def over(self, request):
+        today = date.today()
+        if self.return_on is not None and today > self.return_on:
+            self.is_leave_over = True
+            self.save()
+            messages.warning(request, 'Your leave period has ended.')
         else:
-            self.lv = False
-        self.lv.save()
+            self.is_leave_over = False
+            self.save()
+
 
 class ExecutiveAppointment(models.Model):
     user = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name='execapp')
@@ -321,7 +315,7 @@ class ExecutiveAppointment(models.Model):
     govapp=models.ForeignKey(GovernmentAppointment, on_delete=models.CASCADE, related_name='execgovapp')
     date = models.DateField(null=True,blank=True)
     status = models.CharField(null=True,max_length=300,blank=True)
-    timestamp = models.DateTimeField('date added', auto_now_add=True)
+    created = models.DateTimeField('date added', auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def get_absolute_url(self):
@@ -340,7 +334,7 @@ class Retirement(models.Model):
     status = models.CharField(null=True,max_length=300,blank=True)
     retire=models.BooleanField(null=True, blank=True)
     rtb=models.CharField('retired by', null=True, blank=True,max_length=50)
-    timestamp = models.DateTimeField('date added', auto_now_add=True)
+    created = models.DateTimeField('date added', auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def get_absolute_url(self):
@@ -351,21 +345,17 @@ class Retirement(models.Model):
             return f"{self.user.last_name} {self.user.first_name}"
     
     def rt(self):
-        today=date.today()
-        art=self.profile.age()
-        drt=self.govapp.date_fapt
+        today = date.today()
+        age_at_retirement = self.profile.age()
+        years_of_service = today.year - self.govapp.date_fapt.year if self.govapp.date_fapt else None
 
-        if drt is not None:
-            drt=today.year-drt.year
-
-        if art is not None and art >= 65:
+        if age_at_retirement is not None and age_at_retirement >= 65:
             self.retire = True
             self.rtb = "RETIRE BY DATE OF BIRTH"
-            self.save()
-        elif drt is not None and drt >= 35:
+        elif years_of_service is not None and years_of_service >= 35:
             self.retire = True
             self.rtb = "RETIRE BY DATE OF APPOINTMENT"
-            self.save()
         else:
             self.retire = False
-            self.save()
+
+        self.save()
