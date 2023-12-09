6530b0f0855166adeded66f2f097f36f58382b43
Muhammad Raji <rajisunusi@gmail.com>
2023-12-10 00:19:02 +0100

    logout issue sorted

--- staff/urls.py
+++ staff/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path,include
-from .views import CustomLoginView, ProfileDetailView, DocumentationView, UserRegistrationView
+from .views import CustomLoginView, ProfileDetailView, DocumentationView, UserRegistrationView, CustomLogoutView
 from . import views
 
 urlpatterns = [
@@ -8,5 +8,6 @@ urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('documentation/<int:pk>/', DocumentationView.as_view(), name='doc'),
     path('profile/<str:username>/', ProfileDetailView.as_view(), name='profile_details'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('',include('django.contrib.auth.urls')),
 ]
--- staff/views.py
+++ staff/views.py
@@ -5,8 +5,7 @@ from django.contrib import messages
 from django.utils.decorators import method_decorator
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required, user_passes_test
-from django.core.paginator import Paginator
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from .models import *
 from .forms import *
 from django.contrib.auth import get_user_model
@@ -15,45 +14,36 @@ User = get_user_model()
 
 
 def log_anonymous_required(view_function, redirect_to=None):
-    """
-    Decorator for views that checks if the user is not logged in.
-    """
     if redirect_to is None:
         redirect_to = '/'
-
-    return user_passes_test(
-        lambda u: not u.is_authenticated,
-        login_url=redirect_to
-    )(view_function)
+    return user_passes_test(lambda u: not u.is_authenticated,login_url=redirect_to)(view_function)
 
 
-# @login_required
 def index(request):
     p=Profile.objects.all()
     context={'p':p}
     return render(request, 'index.html',context)
 
+
 @method_decorator(log_anonymous_required, name='dispatch')
 class CustomLoginView(LoginView):
     template_name='login.html'
-
     def get_success_url(self):
-        if self.request.user != self.request.user.is_superuser:
-            return reverse_lazy('profile_details',args=[self.request.user.username])
-        else:
+        if self.request.user.is_superuser:
             return reverse_lazy('index')
+        else:
+            return reverse_lazy('profile_details',args=[self.request.user.username])
+
+
+@method_decorator(login_required, name='dispatch')
+class CustomLogoutView(LogoutView):
+    template_name='logged_out.html'
+
 
 def reg_anonymous_required(view_function, redirect_to=None):
-    """
-    Decorator for views that checks if the user is not registered or is a superuser.
-    """
     if redirect_to is None:
         redirect_to = '/'
-
-    return user_passes_test(
-        lambda u: not u.is_authenticated or u.is_superuser,
-        login_url=redirect_to
-    )(view_function)
+    return user_passes_test(lambda u: not u.is_authenticated or u.is_superuser,login_url=redirect_to)(view_function)
 
 
 @method_decorator(reg_anonymous_required, name='dispatch')
@@ -65,15 +55,9 @@ class UserRegistrationView(CreateView):
     def form_valid(self, form):
         if form.is_valid():
             response = super().form_valid(form)
-            
-            # Ensure the user is created
             user = User.objects.get(username=form.cleaned_data['username'])
-
-            # Create the profile
             profile_instance = Profile(user=user)
             profile_instance.save()
-
-            # Create the government appointment
             govapp_instance = GovernmentAppointment(user=user)
             govapp_instance.save()
             messages.success(self.request, f"Registration for {user.get_full_name()} was successful")
@@ -115,20 +99,12 @@ class DocumentationView(UpdateView):
 class ProfileDetailView(View):
     def get(self, request, *args, **kwargs):
         if request.user.is_superuser:
-            # If the user is a superuser, retrieve the profile based on the username in the URL
-            username_from_url = kwargs.get('username')  # Assuming you pass the username in the URL
+            username_from_url = kwargs.get('username')
             profile = get_object_or_404(Profile, user__username=username_from_url)
             govapp = get_object_or_404(GovernmentAppointment, user__username=username_from_url)
-            # promotion = get_object_or_404(Promotion, user__username=username_from_url)
         else:
-            # If the user is not a superuser, display the profile of the logged-in user
             profile = request.user.profile
             govapp = get_object_or_404(GovernmentAppointment, user=request.user)
-            # promotion = get_object_or_404(Promotion, user=request.user)
 
-        context = {
-            'profile': profile,
-            'govapp': govapp,
-            # 'promotion': promotion,
-        }
+        context = {'profile': profile,'govapp': govapp,}
         return render(request, 'staff/profile_details.html', context)
