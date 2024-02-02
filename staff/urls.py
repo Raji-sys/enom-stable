886b74d0064eac510bea4617db49c37155496e69
Muhammad Raji <rajisunusi@gmail.com>
2024-02-02 19:37:03 +0100

    discipline tested

--- staff/urls.py
+++ staff/urls.py
@@ -4,13 +4,18 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    
     path('login/', CustomLoginView.as_view(), name='login'),
     path('register/', UserRegistrationView.as_view(), name='register'),
+    
     path('documentation/<int:pk>/', DocumentationView.as_view(), name='doc'),
+    
     path('profile/<str:username>/', ProfileDetailView.as_view(), name='profile_details'),
     path('update-user/<int:pk>/', UpdateUserView.as_view(), name='update_user'),
     path('update-profile/<int:pk>/', UpdateProfileView.as_view(), name='update_profile'),
+    
     path('update-govapp/<int:pk>/', UpdateGovappView.as_view(), name='update_govapp'),
+    
     path('qualification/<str:username>/', QualCreateView.as_view(), name='qual'),
     path('qualification/update/<int:pk>/', QualUpdateView.as_view(), name='qual_update'),
     path('qualification/delete/<int:pk>/', QualDeleteView.as_view(), name='qual_delete'),
@@ -23,6 +28,7 @@ urlpatterns = [
     path('discipline/<str:username>/', DisciplineCreateView.as_view(), name='discipline'),
     path('discipline/update/<int:pk>/', DisciplineUpdateView.as_view(), name='discipline_update'),
     path('discipline/delete/<int:pk>/', DisciplineDeleteView.as_view(), name='discipline_delete'),
+    
     path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('',include('django.contrib.auth.urls')),
 ]
