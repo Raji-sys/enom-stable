d190cc419b4cac3882c68cf63c29ec593b917a01
Muhammad Raji <rajisunusi@gmail.com>
2024-02-03 22:57:59 +0100

    enough for today

--- staff/filters.py
+++ staff/filters.py
@@ -4,23 +4,23 @@ from .models import *
 
 
 class GovFilter(django_filters.FilterSet):
-    syear_of_fapt=django_filters.DateFilter(label="F_APT_START",field_name="governmentappointment__date_fapt__year",lookup_expr='lte',widget=forms.DateInput(attrs={'type':'date'}),input_formats=['%d-%m-%Y', '%Y-%m-%d', '%m/%d/%Y'])
-    eyear_of_fapt=django_filters.DateFilter(label="E_APT_END",field_name="governmentappointment__date_fapt__year",lookup_expr='gt',widget=forms.DateInput(attrs={'type':'date'}),input_formats=['%d-%m-%Y', '%Y-%m-%d', '%m/%d/%Y'])
-    year_of_fapt=django_filters.NumberFilter(label="C_YEAR",field_name="governmentappointment__date_fapt__year",lookup_expr='exact')
+    syear_of_fapt=django_filters.DateFilter(label="FIRST START",field_name="governmentappointment__date_fapt__year",lookup_expr='lte',widget=forms.DateInput(attrs={'type':'date'}),input_formats=['%d-%m-%Y', '%Y-%m-%d', '%m/%d/%Y'])
+    eyear_of_fapt=django_filters.DateFilter(label="FIRST END",field_name="governmentappointment__date_fapt__year",lookup_expr='gt',widget=forms.DateInput(attrs={'type':'date'}),input_formats=['%d-%m-%Y', '%Y-%m-%d', '%m/%d/%Y'])
+    # year_of_fapt=django_filters.NumberFilter(label="C_YEAR",field_name="governmentappointment__date_fapt__year",lookup_expr='exact')
 
-    syear_of_capt=django_filters.DateFilter(label="C_APT_START",field_name="governmentappointment__date_capt__year",lookup_expr='lte',widget=forms.DateInput(attrs={'type':'date'}),input_formats=['%d-%m-%Y', '%Y-%m-%d', '%m/%d/%Y'])
-    eyear_of_capt=django_filters.DateFilter(label="C_APT_END",field_name="governmentappointment__date_capt__year",lookup_expr='gt',widget=forms.DateInput(attrs={'type':'date'}),input_formats=['%d-%m-%Y', '%Y-%m-%d', '%m/%d/%Y'])
-    year_of_capt=django_filters.NumberFilter(label="C_YEAR",field_name="governmentappointment__date_capt__year",lookup_expr='exact')
+    syear_of_capt=django_filters.DateFilter(label="CURRRENT START",field_name="governmentappointment__date_capt__year",lookup_expr='lte',widget=forms.DateInput(attrs={'type':'date'}),input_formats=['%d-%m-%Y', '%Y-%m-%d', '%m/%d/%Y'])
+    eyear_of_capt=django_filters.DateFilter(label="CURRENT END",field_name="governmentappointment__date_capt__year",lookup_expr='gt',widget=forms.DateInput(attrs={'type':'date'}),input_formats=['%d-%m-%Y', '%Y-%m-%d', '%m/%d/%Y'])
+    # year_of_capt=django_filters.NumberFilter(label="C_YEAR",field_name="governmentappointment__date_capt__year",lookup_expr='exact')
 
-    dep=django_filters.CharFilter(label="DEPT",field_name="governmentappointment__department",lookup_expr='iexact')
+    dep=django_filters.CharFilter(label="DEPARTMENT",field_name="governmentappointment__department",lookup_expr='iexact')
     cadre=django_filters.CharFilter(label="CADRE",field_name="governmentappointment__cpost",lookup_expr='iexact')
   
     zone=django_filters.CharFilter(label="ZONE",field_name="profile__zone",lookup_expr='iexact')
     state=django_filters.CharFilter(label="STATE",field_name="profile__state",lookup_expr='iexact')
     lga=django_filters.CharFilter(label="LGA",field_name="profile__lga",lookup_expr='iexact')
 
-    qual=django_filters.CharFilter(label="qual",field_name="qualification__qual",lookup_expr='iexact')
-    pro_qual=django_filters.CharFilter(label="pro_qual",field_name="professionalqualification__qual_obtained",lookup_expr='iexact')
+    qual=django_filters.CharFilter(label="QUALIFICATION",field_name="qualification__qual",lookup_expr='iexact')
+    pro_qual=django_filters.CharFilter(label="PRO QUALIFICATION",field_name="professionalqualification__qual_obtained",lookup_expr='iexact')
   
     type_of_cadre=django_filters.CharFilter(label="TYPE",field_name="governmentappointment__type_of_cadre",lookup_expr='iexact')
     due=django_filters.CharFilter(label="DUE",field_name="promotion__due",lookup_expr='iexact')
